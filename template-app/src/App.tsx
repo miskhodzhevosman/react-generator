45b6ea3f54f2bab6d5aa480d2e6b4a3e42806713
miskhodzhevosman <miskhodzhev.osman@gmail.com>
2026-09-18 20:28:31 +0300

    add sidebar and pages

--- template-app/src/App.tsx
+++ template-app/src/App.tsx
@@ -1,9 +1,17 @@
-import MainPage from './modules/entity/MainView.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import MainPage from './modules/entity/entityMainView.jsx'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <MainPage />
+    <div className="app">
+      <Sidebar />
+      <main className="content">
+        <Routes>
+          <Route path="/entity" element={<MainPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
---
+++ template-app/src/components/Sidebar.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+
+type MenuItem = {
+  path: string
+  label: string
+  icon: string
+}
+
+const menuItems: MenuItem[] = [
+  { path: '/entity', label: 'entity', icon: '🏠' },
+]
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="logo">MyApp</div>
+      <nav>
+        {menuItems.map((item) => (
+          <NavLink
+            key={item.path}
+            to={item.path}
+            end={item.path === '/'}
+            className={({ isActive }) =>
+              `nav-link ${isActive ? 'active' : ''}`
+            }
+          >
+            <span className="icon">{item.icon}</span>
+            <span>{item.label}</span>
+          </NavLink>
+        ))}
+      </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
